52294d4ca1e79b7fd8abcbc75790d03187d3558e
Matt Ellis <m.t.ellis@gmail.com>
2018-10-13 10:15:56 +0200

    Minor tweaks

--- Assets/Scripts/TowerDefense/Inspections/PerfContext.cs
+++ Assets/Scripts/TowerDefense/Inspections/PerfContext.cs
@@ -1,31 +1,12 @@
 using System.Collections;
 using UnityEngine;
 
-namespace TowerDefense.Inspections.Perf
+namespace TowerDefense.Inspections
 {
     public class CommonTest : MonoBehaviour
     {
         private Rigidbody2D myRigidBody2D;
 
-        public void Start()
-        {
-            // Coroutines are called frequently, so don't call expensive methods from them
-            StartCoroutine("HotMethod");
-            StartCoroutine(HotMethod2());
-        }
-
-        public IEnumerator HotMethod()
-        {
-            var x = gameObject.GetComponent<Transform>();
-            yield break;
-        }
-        
-        public IEnumerator HotMethod2()
-        {
-            var x = GetComponent<Transform>();
-            yield break;
-        }
-        
         // Update is called frequently, so don't directly or indirectly call expensive methods
         public void Update()
         {
@@ -45,5 +26,25 @@ namespace TowerDefense.Inspections.Perf
         {
             IndirectCostly();
         }
+        
+        public void Start()
+        {
+            // Coroutines are called frequently, so don't call expensive methods from them
+            StartCoroutine("HotMethod");
+            StartCoroutine(HotMethod2());
+        }
+
+        public IEnumerator HotMethod()
+        {
+            var x = gameObject.GetComponent<Transform>();
+            yield break;
+        }
+        
+        public IEnumerator HotMethod2()
+        {
+            var x = GetComponent<Transform>();
+            yield break;
+        }
+        
     }
 }
