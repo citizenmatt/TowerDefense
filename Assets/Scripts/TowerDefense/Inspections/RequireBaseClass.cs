45852cbb4455882cc11233626e6870b9b3e6f0c0
Matt Ellis <m.t.ellis@gmail.com>
2022-03-15 10:09:14 +0000

    Comment out code with a load time warning

--- Assets/Scripts/TowerDefense/Inspections/RequireBaseClass.cs
+++ Assets/Scripts/TowerDefense/Inspections/RequireBaseClass.cs
@@ -4,10 +4,13 @@ using UnityEditor;
 
 namespace TowerDefense.Inspections
 {
-    [CustomEditor(typeof(MyMonoBehaviour))]
-    public class RequiresEditorBaseClass
-    {
-    }
+    // External annotations provide additional context for Unity features, such as using the CustomEditor attribute
+    // requires a base class of Editor
+    // Commented out by default, as this shows as a warning in the Unity editor at load time
+    // [CustomEditor(typeof(MyMonoBehaviour))]
+    // public class RequiresEditorBaseClass
+    // {
+    // }
 }
 
 #endif
